b9779f0b5c748b842b73876ea0575c2197607032
Emer-Rocha <103082600+Emer-Rocha@users.noreply.github.com>
2022-04-14 21:13:39 +0000

    Update App.js

--- MyThirdProject/App.js
+++ MyThirdProject/App.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import {  Text, Image, View } from 'react-native';
 
-function MyCat() {
-  return (
-    <View>
-     <Image source="https://cdn.britannica.com/91/181391-050-1DA18304/cat-toes-paw-number-paws-tiger-tabby.jpg?q=60" 
-  style={{width: 200,height: 200}}
+export default function MyCat() {
 
+  let pic = {uri: "https://cdn.britannica.com/91/181391-050-1DA18304/cat-toes-paw-number-paws-tiger-tabby.jpg?q=60"};
+  return (
+    <View style={{
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center'
+    }} >
+     <Image source={pic} 
+  style={{width: 200,height: 200}}
      />
      <Text>Hello, This is my cat</Text>
     </View>
   );
 }
+
 
 
